5aa113ee3a1f9251735112fb6af69c7872d6a64a
l-yohai <yohan9612@naver.com>
2020-08-15 05:20:42 +0900

    update

--- philo_one/main.c
+++ philo_one/main.c
@@ -6,7 +6,7 @@
 /*   By: yohlee <yohlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/15 00:52:01 by yohlee            #+#    #+#             */
-/*   Updated: 2020/08/15 04:59:20 by yohlee           ###   ########.fr       */
+/*   Updated: 2020/08/15 05:17:18 by yohlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,17 +52,17 @@ typedef struct		s_data
 
 typedef struct		s_philo
 {
-	pthread_t		tid;
+	pthread_t		thread_id;
 	int				id;
 	int				next_id;
-	int				meal_counter;
+	int				count_eat;
 	unsigned long	time_of_last_eat;
 	pthread_mutex_t	last_eat;
 	t_data			*data;
 	t_mutex			*mutex;
 }					t_philo;
 
-unsigned long	get_timestamp_ms(void)
+unsigned long	get_time(void)
 {
 	struct timeval tv;
 
@@ -103,6 +103,13 @@ void			ft_putnbr(unsigned long n)
 		ft_putnbr(n / 10);
 	c = n % 10 + '0';
 	write(1, &c, 1);
+}
+
+int		exit_error(char *s)
+{
+	write(2, s, ft_strlen(s));
+	exit(EXIT_FAILURE);
+	return (EXIT_FAILURE);
 }
 
 int		valid_arguments(char *av[])
@@ -147,68 +154,66 @@ void	*print_exit(t_philo *philo, int index, unsigned long timestamp)
 		ft_putnbr(timestamp - philo->data->time_of_start);
 		write(1, "\t", 1);
 		ft_putnbr(philo->id + 1);
-	}
-	if (index == HAS_DIED)
 		write(1, MSG_DIE, ft_strlen(MSG_DIE));
-	else if (index == ARE_SATIATED)
+	}
+	if (index == ARE_SATIATED)
+	{
 		write(1, MSG_FINISH, ft_strlen(MSG_FINISH));
+		exit(EXIT_SUCCESS);
+	}
 	pthread_mutex_unlock(&philo->mutex->write);
 	return (NULL);
 }
 
-void	*monitor_meals(void *ph)
+void	*monitor_eat(void *philo)
 {
-	t_mutex *mx;
+	t_mutex *mutex;
 
-	mx = ((t_philo *)ph)->mutex;
+	mutex = ((t_philo *)philo)->mutex;
 	while (1)
 	{
-		pthread_mutex_lock(&mx->global_died);
+		pthread_mutex_lock(&mutex->global_died);
 		if (g_philo_died)
 		{
-			pthread_mutex_unlock(&mx->global_died);
+			pthread_mutex_unlock(&mutex->global_died);
 			return (NULL);
 		}
-		pthread_mutex_lock(&mx->global_satiated);
-		if (g_philos_satiated == ((t_philo *)ph)->data->number_of_philosophers)
+		pthread_mutex_lock(&mutex->global_satiated);
+		if (g_philos_satiated == ((t_philo *)philo)->data->number_of_philosophers)
 		{
-			pthread_mutex_unlock(&mx->global_satiated);
+			pthread_mutex_unlock(&mutex->global_satiated);
 			g_philo_died = 1;
-			return (print_exit(ph, ARE_SATIATED, 1));
+			return (print_exit(philo, ARE_SATIATED, 1));
 		}
-		pthread_mutex_unlock(&mx->global_died);
-		pthread_mutex_unlock(&mx->global_satiated);
+		pthread_mutex_unlock(&mutex->global_died);
+		pthread_mutex_unlock(&mutex->global_satiated);
 		usleep(10);
 	}
 }
 
-/*
-** monitoring thread created by each philosopher to monitor it's own health
-*/
-
-void	*monitor_death(void *ph)
+void	*monitor_death(void *philo)
 {
-	t_philo			*philo;
+	t_philo			*p;
 	unsigned long	timestamp;
 
-	philo = ph;
+	p = philo;
 	while (1)
 	{
-		pthread_mutex_lock(&philo->last_eat);
-		if ((timestamp = get_timestamp_ms()) - philo->time_of_last_eat >
-			philo->data->time_to_die)
+		pthread_mutex_lock(&p->last_eat);
+		if ((timestamp = get_time()) - p->time_of_last_eat >
+			p->data->time_to_die)
 		{
-			pthread_mutex_unlock(&philo->last_eat);
-			pthread_mutex_lock(&philo->mutex->global_died);
+			pthread_mutex_unlock(&p->last_eat);
+			pthread_mutex_lock(&p->mutex->global_died);
 			if (!g_philo_died)
 			{
 				g_philo_died = 1;
-				return (print_exit(philo, HAS_DIED, timestamp));
+				return (print_exit(p, HAS_DIED, timestamp));
 			}
-			pthread_mutex_unlock(&philo->mutex->global_died);
+			pthread_mutex_unlock(&p->mutex->global_died);
 			return (NULL);
 		}
-		pthread_mutex_unlock(&philo->last_eat);
+		pthread_mutex_unlock(&p->last_eat);
 		usleep(10);
 	}
 }
@@ -223,7 +228,7 @@ int		print_status(t_philo *philo, int index)
 	}
 	pthread_mutex_lock(&philo->mutex->write);
 	pthread_mutex_unlock(&philo->mutex->global_died);
-	print_msg(philo, index, get_timestamp_ms());
+	print_msg(philo, index, get_time());
 	pthread_mutex_unlock(&philo->mutex->write);
 	return (1);
 }
@@ -257,12 +262,12 @@ int		eat(t_philo *philo)
 		return (0);
 	}
 	pthread_mutex_lock(&philo->last_eat);
-	philo->time_of_last_eat = get_timestamp_ms();
+	philo->time_of_last_eat = get_time();
 	pthread_mutex_unlock(&philo->last_eat);
 	usleep(philo->data->time_to_eat);
 	pthread_mutex_unlock(&philo->mutex->fork[philo->id]);
 	pthread_mutex_unlock(&philo->mutex->fork[philo->next_id]);
-	if (++philo->meal_counter == philo->data->number_of_times_each_philosopher_must_eat)
+	if (++philo->count_eat == philo->data->number_of_times_each_philosopher_must_eat)
 	{
 		pthread_mutex_lock(&philo->mutex->global_satiated);
 		++g_philos_satiated;
@@ -271,17 +276,17 @@ int		eat(t_philo *philo)
 	return (1);
 }
 
-void	*life_cycle(void *ph)
+void	*cycle(void *ph)
 {
 	t_philo		*philo;
-	pthread_t	tid;
+	pthread_t	thread_id;
 
 	philo = ph;
 	philo->next_id = (philo->id == philo->data->number_of_philosophers - 1) ?
 							0 : philo->id + 1;
-	philo->meal_counter = 0;
+	philo->count_eat = 0;
 	philo->time_of_last_eat = philo->data->time_of_start;
-	pthread_create(&tid, NULL, monitor_death, philo);
+	pthread_create(&thread_id, NULL, monitor_death, philo);
 	while (1)
 	{
 		if (!eat(philo))
@@ -292,28 +297,22 @@ void	*life_cycle(void *ph)
 		if (!print_status(philo, IS_THINKING))
 			break ;
 	}
-	pthread_join(tid, NULL);
+	pthread_join(thread_id, NULL);
 	return (NULL);
 }
 
 int		parse(char *av[], t_data *data)
 {
 	if (!valid_arguments(av))
-	{
-		write(2, "Error: only positive numeric arguments are allowed\n", 51);
-		return (0);
-	}
+		return (exit_error("Error: invalid argument. Check argv is positive numeric.\n"));
 	data->number_of_philosophers = ft_atoul(av[1]);
 	if (data->number_of_philosophers < 2)
-	{
-		write(2, "Error: a minimum of two philosophers are required\n", 50);
-		return (0);
-	}
+		return (exit_error("Error: number of philosophers should be at least 2.\n"));
 	data->number_of_times_each_philosopher_must_eat = (av[5]) ? ft_atoul(av[5]) : -1;
 	data->time_to_die = ft_atoul(av[2]);
 	data->time_to_eat = ft_atoul(av[3]) * 1000;
 	data->time_to_sleep = ft_atoul(av[4]) * 1000;
-	return (1);
+	return (EXIT_SUCCESS);
 }
 
 void	cleanup(t_philo *philo)
@@ -342,11 +341,7 @@ int		init(t_philo **philo, t_mutex *mutex, int number_of_philosophers)
 	*philo = NULL;
 	if (!(mutex->fork = malloc(sizeof(*mutex->fork) * number_of_philosophers)) ||
 		!(*philo = malloc(sizeof(**philo) * number_of_philosophers)))
-	{
-		free(mutex->fork);
-		free(*philo);
-		return (1);
-	}
+		return (exit_error("Error: memory allocation.\n"));
 	i = -1;
 	while (++i < number_of_philosophers)
 	{
@@ -358,14 +353,14 @@ int		init(t_philo **philo, t_mutex *mutex, int number_of_philosophers)
 	ret += pthread_mutex_init(&mutex->global_satiated, NULL);
 	(ret != 0) ? cleanup(*philo) : 0;
 	if (ret != 0)
-		return (0);
-	return (1);
+		return (EXIT_SUCCESS);
+	return (EXIT_FAILURE);
 }
 
-void	run_simulation(t_philo *philo, t_data *data, t_mutex *mutex)
+void	run(t_philo *philo, t_data *data, t_mutex *mutex)
 {
 	int			i;
-	pthread_t	tid;
+	pthread_t	thread_id;
 
 	i = -1;
 	while (++i < data->number_of_philosophers)
@@ -374,19 +369,19 @@ void	run_simulation(t_philo *philo, t_data *data, t_mutex *mutex)
 		philo[i].data = data;
 		philo[i].mutex = mutex;
 	}
-	data->time_of_start = get_timestamp_ms();
+	data->time_of_start = get_time();
 	i = -1;
 	while (++i < data->number_of_philosophers)
 	{
-		pthread_create(&philo[i].tid, NULL, life_cycle, &philo[i]);
+		pthread_create(&philo[i].thread_id, NULL, cycle, &philo[i]);
 		(i == 0 && data->number_of_times_each_philosopher_must_eat > 0) ?
-			pthread_create(&tid, NULL, monitor_meals, &philo[i]) : 0;
+			pthread_create(&thread_id, NULL, monitor_eat, &philo[i]) : 0;
 		usleep(50);
 	}
-	(data->number_of_times_each_philosopher_must_eat > 0) ? pthread_join(tid, NULL) : 0;
+	(data->number_of_times_each_philosopher_must_eat > 0) ? pthread_join(thread_id, NULL) : 0;
 	i = -1;
 	while (++i < data->number_of_philosophers)
-		pthread_join(philo[i].tid, NULL);
+		pthread_join(philo[i].thread_id, NULL);
 }
 
 int		main(int ac, char **av)
@@ -396,16 +391,12 @@ int		main(int ac, char **av)
 	t_mutex		mutex;
 
 	if ((ac != 5 && ac != 6))
-	{
-		write(2, "Usage: ./philo_one [nb_of_philos] [time_to_die] ", 48);
-		write(2, "[time_to_eat] [time_to_sleep]\n", 30);
-		write(2, "- optional: [nb_of_req_meals]\n- times in ms\n", 44);
-		return (1);
-	}
+		return (exit_error("Error: invalid argument. Check argc is 5 or 6.\n"));
 	philo = NULL;
-	if (!parse(av, &data) || !init(&philo, &mutex, data.number_of_philosophers))
-		return (1);
-	run_simulation(philo, &data, &mutex);
+	if (parse(av, &data) == EXIT_FAILURE ||\
+			init(&philo, &mutex, data.number_of_philosophers) == EXIT_SUCCESS)
+		return (EXIT_FAILURE);
+	run(philo, &data, &mutex);
 	cleanup(philo);
-	return (0);
+	return (EXIT_SUCCESS);
 }
